f0d44077177f2ce5aadeb77ec77946ea06ed91f3
Zac Smith <zrs3141592@gmail.com>
2020-02-25 01:38:37 -0700

    organize structure

---
+++ src/components/pages/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>
+        header: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Molestiae accusantium eos amet, dolorem explicabo iusto consequuntur
+        quia magni eius deleniti facere quae totam tempora provident cumque.
+        Error laudantium rem praesentium! Illo nesciunt libero praesentium vero
+        nisi? Nemo vel, enim culpa, vitae nihil unde eveniet esse porro placeat,
+        earum sunt eaque laborum? Quis aut exercitationem id hic! Fugiat omnis
+        dolorum beatae! Vitae quis libero eius nisi quas tenetur aliquid odit
+        necessitatibus minima magnam, impedit saepe dignissimos aut animi vel
+        voluptates nobis ducimus veritatis ea voluptas. Error maxime officia
+        alias nemo esse.
+      </p>
+    </div>
+  );
+};
+export default Header;
---
+++ src/components/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignedInNavBar from "../SignedInNavBar.js";
+const Profile = () => {
+  return (
+    <div>
+      <SignedInNavBar />
+      <h3>User Profile goes here</h3>
+      <p>Welcome user! Blah blah blah</p>
+    </div>
+  );
+};
+export default Profile;
