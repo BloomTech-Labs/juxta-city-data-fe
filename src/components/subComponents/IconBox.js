3eca77ebdc128974f043b3273042b1bcc6f31537
jackskim <jackskim29@gmail.com>
2020-02-26 20:59:01 -0800

    Add material-ui components and text content

--- src/components/subComponents/IconBox.js
+++ src/components/subComponents/IconBox.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { styled } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const IconCard = styled(Card)({
+  boxShadow: 'none',
+  height: 250 
+});
+
 const IconBox = props => {
   return (
-    <div>
-      <p>Icon Box</p>
-    </div>
+    <Container>
+      <Grid>
+        <Typography variant="h5" gutterBottom>"Find Ur" Place</Typography>
+        <Typography variant="p" gutterBottom>Findur is a place where you can learn
+          , compare, and receive recommendations on cities across the United States!
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs>
+            <IconCard>
+              <CardContent>
+                <Typography variant="p">Quickly compare up to 3 different cities
+                  side-by-side
+                </Typography>
+              </CardContent>
+            </IconCard>
+          </Grid>
+          <Grid item xs>
+            <IconCard>
+              <CardContent>
+                <Typography variant="p">Search for a city to view all the latest 
+                  information
+                </Typography>
+              </CardContent>
+            </IconCard>
+          </Grid>
+          <Grid item xs>
+            <IconCard>
+              <CardContent>
+                <Typography variant="p">Instantly receive a recommendation for
+                  the best place to live
+                </Typography>
+              </CardContent>
+            </IconCard>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 export default IconBox;
