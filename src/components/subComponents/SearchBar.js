872a29332a0e36b407b89e639d84f5cbbb20d97c
Logan Karnes <logan.karnes98@gmail.com>
2020-02-27 19:12:51 -0800

    fixed the suggerstions to search with the current letters not the previous by moving it into a useEffect

--- src/components/subComponents/SearchBar.js
+++ src/components/subComponents/SearchBar.js
@@ -1,12 +1,65 @@
 import React, {useState, useEffect, useContext} from "react";
 import axios from 'axios';
 import CityContext from '../../contexts/CityContext.js';
+import styled from 'styled-components';
+
+const Search = styled.input`
+width: 600px;
+height: 18px;
+border: 0px
+`
+const Form = styled.form`
+background: lightgrey;
+width: 900px;
+margin: 0 auto;
+display: flex;
+flex-wrap: wrap;
+height: 200px;
+align-items: center;
+justify-content:center;
+`
+const Button = styled.button`
+border: 0px;
+background: white;
+height: 20px;
+`
+const CityDropDown = styled.div`
+  position: absolute;
+  border: 1px solid #d4d4d4;
+  border-bottom: none;
+  border-top: none;
+  z-index: 99;
+  background: white;
+  top: 185px;
+`
+const City = styled.p`
+width: 650px;
+line-height: .75;
+font-size: 14px;
+background: white;
+margin: none;
+border-bottom: 1px solid grey;
+`
 
 const SearchBar = props => {
 const [search, setSearch]= useState("");
 const [cities, setCities]= useState([]);
 const {cityData, setCityData} = useContext(CityContext)
-console.log(cityData)
+useEffect(()=>{
+  if(search.length < 2 || undefined){
+    console.log("waiting")
+    setCities([])
+}else{
+  console.log("searching")
+  let arr = search.split("")
+  arr[0] = arr[0].toUpperCase()
+  let cityWithCaps = arr.join("")
+  axios.get(`https://junta-test.herokuapp.com/search?search=${cityWithCaps}`).then(res => {
+    setCities(res.data)
+  })
+}
+}, [search])
+
 const handleSubmit= e => {
   e.preventDefault()
   axios.get(`https://junta-test.herokuapp.com/data?city=${search}`).then(res => {
@@ -15,18 +68,6 @@ const handleSubmit= e => {
   }
 const handleChange= e => {
   setSearch(e.target.value)
-  if(search.length < 3 || undefined){
-    console.log("waiting")
-    setCities([])
-  }else{
-    console.log("searching")
-    let arr = search.split("")
-    arr[0] = arr[0].toUpperCase()
-    let cityWithCaps = arr.join("")
-    axios.get(`https://junta-test.herokuapp.com/search?search=${cityWithCaps}`).then(res => {
-      setCities(res.data)
-    })
-  }
 }
 
 const handleCityClick=(city)=>{
@@ -35,15 +76,16 @@ setSearch(city);
 }
 
   return (
-    <div>
-      <form autoComplete='off' onSubmit={handleSubmit}>
-        <input type='string' name='city' value={search} onChange={handleChange} />
-        <button type='submit'>Submit</button>
-        {cities.splice(0,10).map(city => (
-          <p onClick={()=>handleCityClick(city)}>{city}</p>
-        ))}
-      </form>
-    </div>
+      <Form autoComplete='off' onSubmit={handleSubmit}>
+        <Search type='string' name='city' value={search} onChange={handleChange} />
+        <Button type='submit'>Search</Button>
+        <CityDropDown>
+          {cities.splice(0,10).map(city => (
+            <City onClick={()=>handleCityClick(city)}>{city}</City>
+          ))}
+        </CityDropDown>
+        
+      </Form>
   );
 };
 export default SearchBar;
