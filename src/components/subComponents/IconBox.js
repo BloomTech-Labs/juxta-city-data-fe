04fcc27afc3220194e6275377e07257ea189f73c
jackskim <jackskim29@gmail.com>
2020-02-28 18:05:21 -0800

    Remove top level Container

--- src/components/subComponents/IconBox.js
+++ src/components/subComponents/IconBox.js
@@ -17,61 +17,59 @@ const IconCard = styled(Card)({
 
 const IconBox = props => {
   return (
-    <Container>
-      <Grid>
-        <Typography variant="h5" gutterBottom>"Find Ur" Place</Typography>
-        <Typography variant="body2" gutterBottom>Findur is a place where you can learn
-          , compare, and receive recommendations on cities across the United States!
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs>
-            <IconCard>
-              <CardMedia
-                component="img"
-                image={CompareIcon}
-                title="Compare Icon"
-                style={{ width: '100px', height: 'auto', margin: '20px auto 5px' }}
-              />
-              <CardContent>
-                <Typography variant="body1">Quickly compare up to 3 different cities
-                  side-by-side
-                </Typography>
-              </CardContent>
-            </IconCard>
-          </Grid>
-          <Grid item xs>
-            <IconCard>
-              <CardMedia
-                component="img"
-                image={SearchIcon}
-                title="Search Icon"
-                style={{ width: '100px', height: 'auto', margin: '20px auto 5px' }}
-              />
-              <CardContent>
-                <Typography variant="body1">Search for a city to view all the latest 
-                  information
-                </Typography>
-              </CardContent>
-            </IconCard>
-          </Grid>
-          <Grid item xs>
-            <IconCard>
-              <CardMedia
-                component="img"
-                image={PinIcon}
-                title="Pin Icon"
-                style={{ width: '100px', height: 'auto', margin: '20px auto 5px' }}
-              />
-              <CardContent>
-                <Typography variant="body1">Instantly receive a recommendation for
-                  the best place to live
-                </Typography>
-              </CardContent>
-            </IconCard>
-          </Grid>
+    <Grid>
+      <Typography variant="h5" gutterBottom>"Find Ur" Place</Typography>
+      <Typography variant="body2" gutterBottom>Findur is a place where you can learn
+        , compare, and receive recommendations on cities across the United States!
+      </Typography>
+      <Grid container>
+        <Grid item xs>
+          <IconCard>
+            <CardMedia
+              component="img"
+              image={CompareIcon}
+              title="Compare Icon"
+              style={{ width: '100px', height: 'auto', margin: '20px auto 5px' }}
+            />
+            <CardContent>
+              <Typography variant="body1">Quickly compare up to 3 different cities
+                side-by-side
+              </Typography>
+            </CardContent>
+          </IconCard>
+        </Grid>
+        <Grid item xs>
+          <IconCard>
+            <CardMedia
+              component="img"
+              image={SearchIcon}
+              title="Search Icon"
+              style={{ width: '100px', height: 'auto', margin: '20px auto 5px' }}
+            />
+            <CardContent>
+              <Typography variant="body1">Search for a city to view all the latest 
+                information
+              </Typography>
+            </CardContent>
+          </IconCard>
+        </Grid>
+        <Grid item xs>
+          <IconCard>
+            <CardMedia
+              component="img"
+              image={PinIcon}
+              title="Pin Icon"
+              style={{ width: '100px', height: 'auto', margin: '20px auto 5px' }}
+            />
+            <CardContent>
+              <Typography variant="body1">Instantly receive a recommendation for
+                the best place to live
+              </Typography>
+            </CardContent>
+          </IconCard>
         </Grid>
       </Grid>
-    </Container>
+    </Grid>
   );
 };
 export default IconBox;
