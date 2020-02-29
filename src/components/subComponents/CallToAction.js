8a4d51084990c019b2f0241a0c3c17bc65c6e3db
jackskim <jackskim29@gmail.com>
2020-02-28 20:41:07 -0800

    Add content and styles to CallToAction component

--- src/components/subComponents/CallToAction.js
+++ src/components/subComponents/CallToAction.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { styled } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 const CallToAction = props => {
   return (
-    <div>
-      <p>Call to action</p>
-    </div>
+    <Grid container style={{ padding: '7% 10%' }}>
+      <Grid item sm={6}>
+      </Grid>
+      <Grid item sm={6} style={{ position: 'relative' }}>
+        <Typography variant="h5" align="left" style={{ float: 'left' }}>
+          Join now and see what the locals love and hate about the city you're
+          interested in 
+        </Typography>
+        <Link to="/signin">
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#8BC34A",
+              color: 'white',
+              float: 'right',
+              marginTop: '15px',
+              width: '180px'
+          }}>
+            Sign Up
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
   );
 };
 export default CallToAction;
