f22ef1bf18c20aa073c63276a85fb7b367922696
Logan Karnes <logan.karnes98@gmail.com>
2020-02-28 20:37:49 -0800

    fixed the styling for the dropdown

--- src/components/subComponents/SearchBar.js
+++ src/components/subComponents/SearchBar.js
@@ -7,6 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const Search = styled.input`
+postition: relative;
 width: 450px;
 height: 30px;
 border: 0px;
@@ -14,11 +15,9 @@ font-size: 20px;
 `
 const Form = styled.form`
 width: 620px;
-background: grey;
-height: 93px;
 margin: 0 auto;
 display: flex;
-z-index: 1;
+flex-wrap: wrap;
 align-items: center;
 justify-content:center;
 `
@@ -28,21 +27,16 @@ background: white;
 height: 30px;
 `
 const CityDropDown = styled.div`
-  position: absolute;
-  border: 1px solid #d4d4d4;
-  border-bottom: none;
-  border-top: none;
-  z-index: 2;
-  background: white;
-  top: 255px;
+
 `
 const City = styled.p`
-width: 450px;
+width: 487px;
 line-height: 1;
 font-size: 20px;
 background: white;
 margin: 0px;
-border: 1px solid whitesmoke;
+background: whitesmoke;
+border: 1px solid lightgrey;
 :hover {
   background: lightgrey;
 }
@@ -52,8 +46,6 @@ const SearchBar = props => {
 const [search, setSearch]= useState("");
 const [cities, setCities]= useState([]);
 const {cityData, setCityData} = useContext(CityContext)
-console.log("Context",cityData)
-
 useEffect(()=>{
   if(search.length < 2 || undefined){
     console.log("waiting")
@@ -77,12 +69,10 @@ const handleChange= e => {
 }
 
 const handleCityClick=(city)=>{
-console.log(city)
 setSearch(city);
 }
 
   return (
-    <div>
       <Form autoComplete='off' onSubmit={handleSubmit}>
         <Search type='string' name='city' value={search} onChange={handleChange} />
         <Button type='submit'><SearchIcon/></Button>
@@ -90,15 +80,8 @@ setSearch(city);
           {cities.length > 1 ? cities.splice(0,6).map(city => (
             <City onClick={()=>handleCityClick(city)}>{city}</City>
           )): <div></div>}
-        </CityDropDown>
+        </CityDropDown>    
       </Form>
-      <Autocomplete
-        options={cities}
-        style={{width: 300}}
-        renderInput={params => <TextField {...params} label='search for city' variant='outlined'/>}
-      />
-    </div>
-      
   );
 };
 export default SearchBar;
