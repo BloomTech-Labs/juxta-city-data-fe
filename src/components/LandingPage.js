6f18d5aea0a90d10877a70d13793c07ef1fc31ac
Logan Karnes <logan.karnes98@gmail.com>
2020-03-03 20:49:58 -0800

    set navbar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,11 +6,13 @@ import styled from "styled-components";
 import Header from "./pages/Header.js";
 import Trending from "./trendingCities/Trending.js";
 import MarketingBox from './MarketingBox.js';
+import NavBar from './NavBar';
 
 
  const LandingPage = (props) => {
       return (
         <div>
+          <NavBar {...props}/>
           <Header/>
           <Trending/>
           <MarketingBox/>
