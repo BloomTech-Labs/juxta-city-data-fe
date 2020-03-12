043f05b8cdd183b1bd0a469b08bfb3e09a0c9e47
Logan Karnes <logan.karnes98@gmail.com>
2020-03-11 20:25:47 -0700

    marketing page styling completed

--- src/components/subComponents/Liveability.js
+++ src/components/subComponents/Liveability.js
@@ -1,38 +1,59 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import LiveabilityScoreIcon from '../bestCities/LiveabilityScoreIcon';
+import LiveabilityScoreIcon from '../../assets/liveabilityscore.png';
+import { makeStyles } from '@material-ui/core/styles';
 
+const styles = makeStyles(theme => ({
+  container: {
+    [theme.breakpoints.down('sm')]: {
+      height: 370,
+      width: '90%'
+    },
+  },
+  h4: {
+    fontWeight: 700,
+    fontSize: 40,
+    [theme.breakpoints.down('sm')]: {
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: 28,
+      padding: 5,
+      margin: '50px 0 0 22px',
+    }
+  },
+  Icon : {
+    [theme.breakpoints.down('sm')]: {
+      width: 45,
+      alignSelf: 'flex-end',
+      marginLeft: '80%'
+    }
+  }
+  
+}))
 const Liveability = props => {
+  const classes = styles();
   return (
     <Container
+      className={classes.container}
       style={{
         maxWidth: 1100,
-        padding: '1.5% 2.5% 1%',
+        padding: '1.5% 2.5% 2%',
         boxShadow:
           ' 0px 1px 12px rgba(0, 0, 0, 0.12), 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 1px 4px rgba(0, 0, 0, 0.01)',
       }}
     >
-      <Container style={{ marginBottom: 50 }}>
-        <Typography
-          variant='h4'
-          align='center'
-          style={{ fontWeight: 700, fontSize: 40 }}
+      <Container className={classes.heading}>
+        <h4
+          className={classes.h4}
         >
           The Liveability Score
-          <LiveabilityScoreIcon
-            height={80}
-            width={60}
-            fontSize={30}
-            style={{
-              marginLeft: '25px',
-              paddingRight: '12px',
-            }}
-          />
-        </Typography>
+          <img className={classes.Icon} src={LiveabilityScoreIcon}/>
+
+        </h4>
       </Container>
       <Container>
-        <Typography variant='body1' style={{ lineHeight: 1.3 }}>
+        <Typography variant='body1' style={{ lineHeight: 1.3}}>
           The Liveability score was designed to help you find the best places to
           live. It is created using a unique algorithm that takes into account
           over 15 characteristics including nearby population change, cost of
