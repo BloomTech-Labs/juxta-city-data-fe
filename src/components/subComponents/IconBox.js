cea1c5bb81e7b473b020728ecdd492721b3a915c
Logan Karnes <logan.karnes98@gmail.com>
2020-03-16 19:42:36 -0700

    fixed livability score

--- src/components/subComponents/IconBox.js
+++ src/components/subComponents/IconBox.js
@@ -3,6 +3,7 @@ import CompareIcon from '../../assets/compare.png';
 import PinIcon from '../../assets/pin.png';
 import SearchIcon from '../../assets/search.png';
 import { makeStyles } from '@material-ui/core/styles';
+import {Container} from '@material-ui/core';
 
 
 const styles = makeStyles(theme => ({
@@ -11,6 +12,9 @@ const styles = makeStyles(theme => ({
     margin: '0 auto 65px auto',
     boxShadow: '0px 1px 12px rgba(0, 0, 0, 0.12), 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 1px 4px rgba(0, 0, 0, 0.01)',
     maxWidth: 1100,
+    [theme.breakpoints.down('md')]: {
+      maxWidth: 900,
+    },
     [theme.breakpoints.down('sm')]: {
       height: 617,
       width: '90%'
@@ -75,7 +79,7 @@ const styles = makeStyles(theme => ({
   },
   img: {
     margin: '0 auto',
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('xs')]: {
       width: '20%',
       margin: 0
     }
@@ -85,38 +89,38 @@ const styles = makeStyles(theme => ({
 const IconBox = props => {
   const classes = styles();
   return (
-    <div className={classes.root}>
-      <div className={classes.header}>
-        <h4 className={classes.h4}>"Find Ur" place</h4>
-      <p className={classes.intro}>Findur is a place where you can learn
-        , compare, and receive recommendations on cities across the United States!
-      </p>
-      </div>
-      
-      <div className={classes.IconBox}>
-        <div className={classes.IconCard}>
-          <img
-            className={classes.img}
-            src={CompareIcon}
-          />
-            <p className={classes.body}>Quickly compare up to 3 different cities
-              side-by-side
-            </p>
+      <div className={classes.root}>
+        <div className={classes.header}>
+          <h4 className={classes.h4}>"Find Ur" place</h4>
+        <p className={classes.intro}>Findur is a place where you can learn
+          , compare, and receive recommendations on cities across the United States!
+        </p>
         </div>
-
         
-        <div className={classes.IconCard}>
-        <img className={classes.img} src={SearchIcon}/>
-          <p className={classes.body}>Search for a city to view all the latest 
-information </p>
-        </div>
+        <div className={classes.IconBox}>
+          <div className={classes.IconCard}>
+            <img
+              className={classes.img}
+              src={CompareIcon}
+            />
+              <p className={classes.body}>Quickly compare up to 3 different cities
+                side-by-side
+              </p>
+          </div>
 
-        <div className={classes.IconCard}> 
-          <img className={classes.img} src={PinIcon}/>
-          <p className={classes.body}>Instantly receive a recommendation for the best place to live</p>  
+          
+          <div className={classes.IconCard}>
+          <img className={classes.img} src={SearchIcon}/>
+            <p className={classes.body}>Search for a city to view all the latest 
+  information </p>
+          </div>
+
+          <div className={classes.IconCard}> 
+            <img className={classes.img} src={PinIcon}/>
+            <p className={classes.body}>Instantly receive a recommendation for the best place to live</p>  
+          </div>
         </div>
-      </div>
-    </div>     
+      </div>      
   );
 };
 export default IconBox;
