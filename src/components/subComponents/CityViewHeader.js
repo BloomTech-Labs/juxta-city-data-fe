3e5d3d664edf51f3da766ba182af1c9680c7899b
Logan Karnes <logan.karnes98@gmail.com>
2020-03-18 20:42:41 -0700

    added the handlefavorite function to allow the user to favorite a city they are viewing

--- src/components/subComponents/CityViewHeader.js
+++ src/components/subComponents/CityViewHeader.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import axios from 'axios';
 import styled from 'styled-components'
 import fullheart from '../../assets/fullheart.png'
 import emptyheart from '../../assets/emptyheart.png'
 
 import { makeStyles } from '@material-ui/core/styles';
 import { useMediaQuery } from '@material-ui/core';
-
+import UserContext from '../../contexts/UserContext';
 const styles = makeStyles(theme => ({
     root: {
         height: 350,
@@ -40,11 +41,29 @@ const styles = makeStyles(theme => ({
 
 export default function CityViewHeader(props){
     const classes = styles();
+    const {userData, setUserData} = useContext(UserContext);
+    useEffect(()=> {
+    
+    }, [])
+    const handleFavorite = e => {
+        e.preventDefault();
+        if(userData.name && !userData.favorites.includes(props.cityData.city)){
+            axios.post('', {user_id: userData.id, city_id: props.cityData.id}).then(res=> {
+                console.log(res, 'favorite completed!')
+            })
+        }else if(userData.name && userData.favorites.includes(props.cityData.city)){
+            axios.delete('', {user_id: userData.id, city_id: props.cityData.id}).then(res=> {
+                console.log(res, 'unfavorite completed!')
+            })
+        }else{
+            props.history.push('/signup')
+        }
+    }
     return(
         <div className={classes.root}>
             <div className={classes.HeadingBox}>
-                <img className={classes.Heart} src={localStorage.getItem('okta-token-storage')? fullheart: emptyheart}/>
-               <h3 className={classes.Heading}>{props.cityData.city}</h3> 
+                <img className={classes.Heart} onClick={handleFavorite} src={!userData.favorites ? emptyheart : !props.cityData.city.includes(props.cityData.city) ? fullheart: emptyheart}/>
+    <h3 className={classes.Heading}>{props.cityData.city}</h3> 
             </div>
             <p className={classes.Note}>Population: {props.cityData.population}</p>
         </div>
