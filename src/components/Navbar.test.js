cc836050d959ff0d317b3acea6baa84da6689d58
jackskim <jackskim29@gmail.com>
2020-04-10 20:30:16 -0700

    Fix Navbar test

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -3,46 +3,65 @@ import { Route } from 'react-router-dom';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import Navbar from './Navbar.js';
+import UserContext from '../contexts/UserContext.js';
 
 it('renders without crashing', () => {
-  render(
-    <Router>
-      <Route
-        path="/"
-        render={({ history, location }) => (
-          <Navbar history={history} location={location} /> 
-        )}
-     />
-    </Router>
-  );
-});
+  let userData = {};
+  const setUserData = data => userData = data;
 
-it('renders About link', () => {
-  const { getByText } = render(
-    <Router>
-      <Route
+  render(
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <Router>
+        <Route
           path="/"
           render={({ history, location }) => (
             <Navbar history={history} location={location} /> 
           )}
        />
-    </Router>
+      </Router>
+    </UserContext.Provider>
+  );
+});
+
+it('renders logo link', () => {
+  let userData = {};
+  const setUserData = data => userData = data;
+
+  const { getByAltText } = render(
+
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <Router>
+        <Route
+            path="/"
+            render={({ history, location }) => (
+              <Navbar history={history} location={location} /> 
+            )}
+         />
+      </Router>
+    </UserContext.Provider>
   );
 
-  const linkElement = getByText(/about/i);
-  expect(linkElement).toBeInTheDocument();
+  const logo = getByAltText(/find ur city logo/i);
+  expect(logo).toBeInTheDocument();
 });
 
 it('renders sign in / sign up link', () => {
+  let userData = {};
+  const setUserData = data => userData = data;
+
   const { getByText } = render(
-    <Router>
-      <Route
-          path="/"
-          render={({ history, location }) => (
-            <Navbar history={history} location={location} /> 
-          )}
-       />
-    </Router>);
+
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <Router>
+        <Route
+            path="/"
+            render={({ history, location }) => (
+              <Navbar history={history} location={location} /> 
+            )}
+         />
+      </Router>
+    </UserContext.Provider>
+  );
   const linkElement = getByText(/sign in \/ sign up/i);
   expect(linkElement).toBeInTheDocument();
 });
