836433335a52d019771c8c311ec1ae76fdc22aad
jackskim <jackskim29@gmail.com>
2020-04-13 19:34:11 -0700

    Add tests for Header and Profile pages

---
+++ src/components/pages/__tests__/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+import { render } from '@testing-library/react';
+import Header from '../Header';
+import CityContext from '../../../contexts/CityContext';
+
+jest.mock('axios', () => ({
+  get: jest.fn()
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: jest.fn()
+  })
+}));
+
+it('renders heading and search bar', () => {
+  let cityData = '';
+  const setCityData = data => cityData = data;
+
+  const { getByPlaceholderText, getByText } = render(
+    <CityContext.Provider value={{setCityData}}>
+      <Header />
+    </CityContext.Provider>
+  );
+
+  expect(getByText(/find the perfect/i)).toBeInTheDocument();
+  expect(getByPlaceholderText(/search for a city/i)).toBeInTheDocument();
+
+});
