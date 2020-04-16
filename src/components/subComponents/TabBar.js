d6c717bb066a1681a1a52f845ee5caa47422b7a3
jackskim <jackskim29@gmail.com>
2020-04-15 20:59:25 -0700

    Add data-testids

--- src/components/subComponents/TabBar.js
+++ src/components/subComponents/TabBar.js
@@ -1,66 +1,72 @@
-import React, {useState} from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs'
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { useMediaQuery } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
 
-
-const styles = makeStyles(theme => ({
-    container: {
-        position: 'sticky',
-        top: 80,
-        [theme.breakpoints.down('sm')]: {
-            position: 'fixed',
-            top: 80,
-        },
+const styles = makeStyles((theme) => ({
+  container: {
+    position: "sticky",
+    top: 80,
+    [theme.breakpoints.down("sm")]: {
+      position: "fixed",
+      top: 80,
     },
-    Tabs : {
-        background: '#2196F3',
-        fontStyle: 'normal',
-        fontWeight: 500,
-        fontSize: 24,
-        lineHeight: 28,
-        color: 'white',
+  },
+  Tabs: {
+    background: "#2196F3",
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: 24,
+    lineHeight: 28,
+    color: "white",
+  },
+}));
+
+export default function TabBar(props) {
+  const [value, setValue] = useState("Population");
+  const classes = styles();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    const ele = document.getElementById(newValue);
+
+    if (ele) {
+      const offset = ele.offsetTop;
+      window.scrollTo(0, offset - 190);
     }
-}))
+  };
 
-export default function TabBar(props){
-const [value, setValue] = useState('Population');
-const classes = styles();
-
-const handleChange = (event, newValue) => {
-    setValue(newValue)
-    const ele = document.getElementById(newValue)
-    
-    if(ele){
-        const offset = ele.offsetTop
-        window.scrollTo(0, offset-190);
-    } 
-}
-
-    return(
-        <div className={classes.container}>
-            <AppBar position="sticky" className='classes.AppBar' color="default">
-            <Tabs
-            className={classes.Tabs}
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            variant="scrollable"
-            scrollButtons="auto"
-            aria-label="scrollable auto tabs example"
-            
-            >
-                <Tab label="Population" value="Population" />
-                <Tab label="Climate" value="Climate"/>
-                <Tab label="Economy" value="Economy"/>
-                <Tab label="Cost of Living" value="Cost of Living"/>
-            </Tabs>
-            
-        </AppBar>
-        </div>
-        
-    )
+  return (
+    <div className={classes.container}>
+      <AppBar
+        data-testid="appbar"
+        position="sticky"
+        className="classes.AppBar"
+        color="default"
+      >
+        <Tabs
+          data-testid="tabs"
+          className={classes.Tabs}
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab
+            label="Population"
+            value="Population"
+            data-testid="population-tab"
+          />
+          <Tab label="Climate" value="Climate" data-testid="climate-tab" />
+          <Tab label="Economy" value="Economy" />
+          <Tab label="Cost of Living" value="Cost of Living" />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
 }
