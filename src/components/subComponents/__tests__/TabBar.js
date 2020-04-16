b7bd2400e800f913f9bab5748cbf185374f1b3b3
jackskim <jackskim29@gmail.com>
2020-04-15 20:58:40 -0700

    Add test for active tab based on clicks

--- src/components/subComponents/__tests__/TabBar.js
+++ src/components/subComponents/__tests__/TabBar.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import TabBar from "../TabBar.js";
 
 it("should render tabs with values", () => {
-  const { getByText } = render(<TabBar />);
+  const { getByText, getByTestId } = render(<TabBar />);
+
+  const tabs = getByTestId("tabs");
+  expect(tabs).toBeInTheDocument();
 
   const populationTab = getByText(/population/i);
   const climateTab = getByText(/climate/i);
@@ -14,4 +17,22 @@ it("should render tabs with values", () => {
   expect(climateTab).toBeInTheDocument();
   expect(economyTab).toBeInTheDocument();
   expect(colTab).toBeInTheDocument();
+});
+
+it("should toggle the 'Mui-selected' class when a different tab is clicked", () => {
+  const { getByTestId } = render(<TabBar />);
+
+  const populationTab = getByTestId("population-tab");
+  expect(populationTab).toBeInTheDocument();
+
+  const climateTab = getByTestId("climate-tab");
+  expect(climateTab).toBeInTheDocument();
+
+  expect([...populationTab.classList]).toContain("Mui-selected");
+  expect([...climateTab.classList]).not.toContain("Mui-selected");
+
+  fireEvent.click(climateTab);
+
+  expect([...populationTab.classList]).not.toContain("Mui-selected");
+  expect([...climateTab.classList]).toContain("Mui-selected");
 });
