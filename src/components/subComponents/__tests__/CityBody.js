89df88110f3a6e82783317576261e012533fb5ba
jackskim <jackskim29@gmail.com>
2020-04-16 19:21:05 -0700

    Update CityBody test

--- src/components/subComponents/__tests__/CityBody.js
+++ src/components/subComponents/__tests__/CityBody.js
@@ -4,32 +4,6 @@ import CityContent from "../CityContent.js";
 import TabBar from "../TabBar.js";
 import CityBody from "../CityBody.js";
 
-it("should render tabs and headers with values", () => {
-  const cityData = {
-    population_desc: "Test description",
-    climate_desc: "Test climate",
-    economy_desc: "Test economy",
-    living_cost_desc: "Test living cost",
-  };
-
-  const { getAllByText, getByTestId } = render(
-    <CityBody cityData={cityData} />
-  );
-
-  const tabs = getByTestId("tabs");
-  expect(tabs).toBeInTheDocument();
-
-  const population = getAllByText(/population/i);
-  const climate = getAllByText(/climate/i);
-  const economy = getAllByText(/economy/i);
-  const costOfLiving = getAllByText(/cost of living/i);
-
-  expect(population).toHaveLength(2);
-  expect(climate).toHaveLength(3);
-  expect(economy).toHaveLength(3);
-  expect(costOfLiving).toHaveLength(2);
-});
-
 it("should render four description boxes with data from props", () => {
   const cityData = {
     population_desc: "Test description",
