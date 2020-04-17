265ed057a1bd41aad162cf6db53cb911f5810e15
jackskim <jackskim29@gmail.com>
2020-04-16 19:22:03 -0700

    Update alt text of icon in test

--- src/components/subComponents/__tests__/CityViewHeader.js
+++ src/components/subComponents/__tests__/CityViewHeader.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import CityViewHeader from '../CityViewHeader';
-import UserContext from '../../../contexts/UserContext';
+import React from "react";
+import { render } from "@testing-library/react";
+import CityViewHeader from "../CityViewHeader";
+import UserContext from "../../../contexts/UserContext";
 
-test('all data is rendered', ()=> {
-    const fakeCity = 'LoganVille'
-    const fakePopulation = '12743' 
-    let userData = {};
-    const setUserData = data => userData = data;
+test("all data is rendered", () => {
+  const fakeCity = "LoganVille";
+  const fakePopulation = "12743";
+  let userData = {};
+  const setUserData = (data) => (userData = data);
 
-    const comp = render(
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <CityViewHeader cityData={{city: fakeCity, population: fakePopulation}}/>
-      </UserContext.Provider>
-    )
-    const image = comp.getByAltText('favorite icon')
-    image.src='test.png'
-    //expect(comp.textContent).toMatch(fakePopulation)
-    //expect(comp.textContent).toMatch(fakeCity)
-    expect(image.src).toMatch('test.png')
-})
+  const comp = render(
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <CityViewHeader
+        cityData={{ city: fakeCity, population: fakePopulation }}
+      />
+    </UserContext.Provider>
+  );
+  const image = comp.getByAltText("heart icon");
+  image.src = "test.png";
+  //expect(comp.textContent).toMatch(fakePopulation)
+  //expect(comp.textContent).toMatch(fakeCity)
+  expect(image.src).toMatch("test.png");
+});
