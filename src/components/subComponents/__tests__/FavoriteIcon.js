331f82d8cbb1e580c71ceb18e56c0c01040f12da
Logan Karnes <logan.karnes98@gmail.com>
2020-04-18 14:29:27 -0700

    added the FavoriteIcon test

---
+++ src/components/subComponents/__tests__/FavoriteIcon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+import {render, fireEvent} from '@testing-library/react';
+import FavoriteIcon from '../FavoriteIcon';
+import UserContext from '../../../contexts/UserContext';
+
+
+it('renders the Icon', ()=> {
+    let cityData = {id: 2, city: 'Seattle, Washington'}
+    let userData = {id: 2, username: 'Jake', favorites:[{id:235, city: 'Seattle, Washington'}]}
+    let setUserData = (data) => userData = data;
+    let comp = render(
+        <UserContext.Provider value={{userData, setUserData}}>
+            <FavoriteIcon cityData={cityData}/>
+        </UserContext.Provider>  
+    )
+    const image = comp.getByAltText(/heart icon/i)
+    expect(image).toBeInTheDocument();
+    expect(image.src).toEqual('http://localhost/emptyheart.png')
+    fireEvent.click(image);
+    expect(image.src).toEqual('http://localhost/fullheart.png')
+    fireEvent.click(image);
+    expect(image.src).toEqual('http://localhost/emptyheart.png')
+})
