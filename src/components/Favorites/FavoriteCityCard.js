f173b0040244a95732fc74d73f5bb85ed12773df
jackskim <jackskim29@gmail.com>
2020-04-29 19:20:14 -0700

    Add cursor hover

--- src/components/Favorites/FavoriteCityCard.js
+++ src/components/Favorites/FavoriteCityCard.js
@@ -1,41 +1,47 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import FavoriteIcon from '../subComponents/FavoriteIcon';
-const styles = makeStyles(theme => ({
-    root: {
-        width: '50%',
-        display: 'flex',
-        alignItems: 'center',
-        [theme.breakpoints.down('md')]: {
-            width: '90%'
-          },
-        '&:hover': {
-            background: 'whiteSmoke'
-        }
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import FavoriteIcon from "../subComponents/FavoriteIcon";
+const styles = makeStyles((theme) => ({
+  root: {
+    width: "50%",
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      width: "90%",
     },
-    cityName: {
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        fontSize: 18,
-        
+    "&:hover": {
+      background: "whiteSmoke",
+      cursor: "pointer",
     },
-    image: {
-        padding: 10
-    }
-}))
-export default function FavoiriteCityCard(props){
-const classes = styles()
-const history = useHistory();
+  },
+  cityName: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  image: {
+    padding: 10,
+  },
+}));
+export default function FavoiriteCityCard(props) {
+  const classes = styles();
+  const history = useHistory();
 
-const handleClick = e => {
-    localStorage.setItem('cityName', props.cityData.city)
-    history.push('/cityview')
-}
-    return(
-        <div className={classes.root}>
-            <FavoriteIcon class={classes.image} {...props} cityData={props.cityData} />
-            <p className={classes.cityName} onClick={handleClick}>{props.cityData.city}</p>
-        </div>
-    )
+  const handleClick = (e) => {
+    localStorage.setItem("cityName", props.cityData.city);
+    history.push("/cityview");
+  };
+  return (
+    <div className={classes.root}>
+      <FavoriteIcon
+        class={classes.image}
+        {...props}
+        cityData={props.cityData}
+      />
+      <p className={classes.cityName} onClick={handleClick}>
+        {props.cityData.city}
+      </p>
+    </div>
+  );
 }
