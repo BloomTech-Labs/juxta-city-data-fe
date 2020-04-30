91055265c580b32b45d01129d03f2d7d6e28c1ce
jackskim <jackskim29@gmail.com>
2020-04-29 19:11:08 -0700

    Update styles

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,36 +1,65 @@
 import React, { useState, useContext } from "react";
-import axios from 'axios';
-import {styles} from './authStlyes';
-import {createUserContext} from '../../functions';
-import UserContext from '../../contexts/UserContext';
+import axios from "axios";
+import { styles } from "./authStlyes";
+import { createUserContext } from "../../functions";
+import UserContext from "../../contexts/UserContext";
 
-
-export default function SignIn(props){
-  const [form, setForm] = useState({})
+export default function SignIn(props) {
+  const [form, setForm] = useState({});
   const classes = styles();
-  const {setUserData} = useContext(UserContext)
-  const handleChange = e => {
-    setForm({...form, [e.target.name] : e.target.value})
-  }
-  const handleSubmit = e => {
+  const { setUserData } = useContext(UserContext);
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://production-juxta-city-be.herokuapp.com/api/auth/signin', form).then(res => {
-        localStorage.setItem('token', res.data.token)
-        props.history.push('/dashboard')
-  }).catch(err => {
-    console.log(err)
-    document.getElementById('signin-error').style.display = 'block';
-  })
-  }
+    axios
+      .post(
+        "https://production-juxta-city-be.herokuapp.com/api/auth/signin",
+        form
+      )
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        props.history.push("/dashboard");
+      })
+      .catch((err) => {
+        console.log(err);
+        document.getElementById("signin-error").style.display = "block";
+      });
+  };
   return (
-    <div className={classes.box} style={{background: '#8BC34A'}}>
-      <h3 className={classes.h3} style={{color: 'white'}}>Sign In</h3>
+    <div className={classes.box} style={{ background: "#8BC34A" }}>
+      <h3 className={classes.h3} style={{ color: "white" }}>
+        Sign In
+      </h3>
       <form onSubmit={handleSubmit} className={classes.form}>
-        <p id='signin-error' style={{display: 'none', color: 'red'}}>Sorry User Not Found</p>
-        <input className={classes.inputs} type="text" id="username2" name="username" placeholder='username' value={form.username} onChange={handleChange} required/>
-        <input className={classes.inputs} type="password" id="password2" name="password" placeholder='password' value={form.password} onChange={handleChange} required/>
-        <button className={classes.submit} style={{background: '#2196F3'}}>Submit</button>
+        <p id="signin-error" style={{ display: "none", color: "red" }}>
+          Sorry User Not Found
+        </p>
+        <input
+          className={classes.inputs}
+          type="text"
+          id="username2"
+          name="username"
+          placeholder="Username"
+          value={form.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className={classes.inputs}
+          type="password"
+          id="password2"
+          name="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
+        <button className={classes.submit} style={{ background: "#2196F3" }}>
+          Submit
+        </button>
       </form>
     </div>
-  )
+  );
 }
