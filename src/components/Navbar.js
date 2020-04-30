d987bc260d5cd909ca8efb00821b366cf1fde99c
jackskim <jackskim29@gmail.com>
2020-04-29 18:46:24 -0700

    Remove unused useEffect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import "../App.css";
