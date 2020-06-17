d464fa99d8176c4c20d1d76573969be82f5dea28
Conary36 <cbeckford144@gmail.com>
2020-06-16 23:36:11 -0400

    testing Edit user-first attempt

--- src/components/pages/EditUser.js
+++ src/components/pages/EditUser.js
@@ -5,7 +5,6 @@ const EditUser = props => {
 
     const handleInputChange = event => {
         const { name, value } = event.target
-
         setUser({ ...user, [name]: value })
     }
 
@@ -13,31 +12,30 @@ const EditUser = props => {
         setUser(props.currentUser)
     }, [props])
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.updateUser(user.id, user)
+    }
+    const {email, first_name, last_name, dob, address, city, state, zip} = user
 
     return (
-        <form
-            onSubmit={event => {
-                event.preventDefault()
-
-                props.updateUser(user.id, user)
-            }}
-        >
+        <form onSubmit={handleSubmit}>
             <label>Email</label>
-            <input type="text" name="email" value={user.email} onChange={handleInputChange} />
+            <input type="text" name="email" value={email} onChange={handleInputChange} />
             <label>First Name</label>
-            <input type="text" name="first_name" value={user.first_name} onChange={handleInputChange} />
+            <input type="text" name="first_name" value={first_name} onChange={handleInputChange} />
             <label>Last Name</label>
-            <input type="text" name="last_name" value={user.last_name} onChange={handleInputChange} />
+            <input type="text" name="last_name" value={last_name} onChange={handleInputChange} />
             <label>Date Of Birth</label>
-            <input type="text" name="dob" value={user.dob} onChange={handleInputChange} />
+            <input type="text" name="dob" value={dob} onChange={handleInputChange} />
             <label>Address</label>
-            <input type="text" name="address" value={user.address} onChange={handleInputChange} />
+            <input type="text" name="address" value={address} onChange={handleInputChange} />
             <label>City</label>
-            <input type="text" name="city" value={user.city} onChange={handleInputChange} />
+            <input type="text" name="city" value={city} onChange={handleInputChange} />
             <label>State</label>
-            <input type="text" name="state" value={user.state} onChange={handleInputChange} />
+            <input type="text" name="state" value={state} onChange={handleInputChange} />
             <label>Zip</label>
-            <input type="number" name="zip" value={user.zip} onChange={handleInputChange} />
+            <input type="number" name="zip" value={zip} onChange={handleInputChange} />
             <button>Update user</button>
             <button onClick={() => props.setEditing(false)} className="button muted-button">
                 Cancel
