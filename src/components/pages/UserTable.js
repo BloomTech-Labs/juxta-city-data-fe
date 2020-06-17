c852c1f5f378e1bb5945364162cb0bc2818d565d
Conary36 <cbeckford144@gmail.com>
2020-06-16 23:26:25 -0400

    second test userTable

--- src/components/pages/UserTable.js
+++ src/components/pages/UserTable.js
@@ -6,6 +6,7 @@ import CardActions from '@material-ui/core/CardActions';
 import { IconButton } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import UserTableData from './UsetTableData'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,49 +29,6 @@ const useStyles = makeStyles((theme) => ({
     
 }));
 
-const userTableData = (props) => {
-   
-       
-    if (props.userData.length > 0){
-        return(
-            props.userData.map(i => (
-                <tr key={i.id}>
-                    <td>{i.email}</td>
-                    <td>{i.first_name}</td>
-                    <td>{i.last_name}</td>
-                    <td>{i.dob}</td>
-                    <td>{i.address}</td>
-                    <td>{i.city}</td>
-                    <td>{i.state}</td>
-                    <td>{i.zip}</td>
-                    <td>
-                        <CardActions>
-
-                            <IconButton arial-label="edit" onClick={() => {
-                                props.editRow(i.id)
-                            }}
-                                className="button muted-button">
-                                <EditIcon />
-                            </IconButton>
-                            <IconButton arial-label="delete" onClick={() => props.deleteUser(i.id)} className="button muted-button">
-                                <DeleteIcon />
-                            </IconButton>
-                        </CardActions>
-                    </td>
-                </tr>
-            )))
-    }else{
-        return (
-            <tr>
-                <td colSpan={8}>Profile Empty!</td>
-            </tr>
-        )
-    }
-                        
-       
-
-
-}
 
     const UserTable = (props) => {
         console.log(props.userData);
@@ -94,8 +52,9 @@ const userTableData = (props) => {
                 </tr>
             </thead>
            
+
             <tbody>
-             {userTableData()}
+           <userTableData props={props}/>
            
             </tbody>
         </table>
