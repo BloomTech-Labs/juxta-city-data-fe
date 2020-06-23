2bd293b3f9b79b2e1c68dd81f94d902bd9de3798
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-23 14:22:18 -0700

    Refactored Add Profile components

--- src/components/pages/AddProfileForm.js
+++ src/components/pages/AddProfileForm.js
@@ -1,100 +1,19 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(3),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
+import formStyles from "./formStyles";
+import ProfileInputs from "./ProfileInputs";
 
 function AddProfileForm({ handleSubmit, addHandleChange }) {
-  const classes = useStyles();
+  const classes = formStyles();
 
   return (
     <Grid item xs={12}>
       <Paper className={classes.paper}>
-        <form
-          onSubmit={handleSubmit}
-          className={classes.root}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            label="First Name"
-            variant="outlined"
-            type="text"
-            name="first_name"
-            onChange={addHandleChange}
-          />
-
-          <TextField
-            label="Last Name"
-            variant="outlined"
-            type="text"
-            name="last_name"
-            onChange={addHandleChange}
-          />
-
-          <TextField
-            label="Birthdate"
-            variant="outlined"
-            type="date"
-            name="dob"
-            className={classes.textField}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={addHandleChange}
-          />
-
-          <TextField
-            label="Address"
-            variant="outlined"
-            type="text"
-            name="address"
-            onChange={addHandleChange}
-          />
-
-          <TextField
-            label="City"
-            variant="outlined"
-            type="text"
-            name="city"
-            onChange={addHandleChange}
-          />
-
-          <TextField
-            label="State"
-            variant="outlined"
-            type="text"
-            name="state"
-            onChange={addHandleChange}
-          />
-
-          <TextField
-            label="Zip code"
-            variant="outlined"
-            type="number"
-            name="zip"
-            onChange={addHandleChange}
-          />
-          <Button type="submit" variant="outlined" color="primary" size="large">
-            Add profile
-          </Button>
-        </form>
+        <ProfileInputs
+          handleSubmit={handleSubmit}
+          addHandleChange={addHandleChange}
+        />
       </Paper>
     </Grid>
   );
---
+++ src/components/pages/ProfileInputs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import SelectState from "./SelectState";
+import TextField from "@material-ui/core/TextField";
+import formStyles from "./formStyles";
+
+function ProfileInputs({ handleSubmit, addHandleChange }) {
+  const classes = formStyles();
+
+  return (
+    <form onSubmit={handleSubmit} className={classes.root}
+      noValidate autoComplete="off"
+    >
+      <TextField label="First Name" type="text" name="first_name" onChange={addHandleChange} />
+      <TextField label="Last Name" type="text" name="last_name" onChange={addHandleChange} />
+      <TextField label="Birthdate"type="date"name="dob" onChange={addHandleChange}
+        InputLabelProps={{shrink: true}} />
+      <TextField label="Address"type="text"name="address" onChange={addHandleChange} />
+      <TextField label="City" type="text" name="city" onChange={addHandleChange} />
+      <SelectState addHandleChange={addHandleChange} />
+      <TextField label="Zip code" type="number" name="zip" onChange={addHandleChange} />
+      <Button type="submit" variant="outlined" color="primary" className={classes.button}>Add profile</Button>
+    </form>
+  );
+}
+export default ProfileInputs;
