01ad644a389f481b1f05b58526be7b8b04ff2ce5
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-23 20:35:10 -0700

    Created a profile directory  and moved all the profile components in it.

--- src/components/pages/profile/InfoList.js
+++ src/components/pages/profile/InfoList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profileInfoStyle from "./profileStyles/profileInfoStyle";
+import profileInfoStyle from "../profileStyles/profileInfoStyle";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
