f3c144820989b7f102af85e027f0c2750595b3f5
Sean Birmingham <seannbirmingham98@gmail.com>
2020-07-05 20:09:48 -0400

    added functionality to upload and remove a profile image

--- src/components/pages/profile/InfoList.js
+++ src/components/pages/profile/InfoList.js
@@ -1,41 +1,49 @@
-import React from "react";
-import profileInfoStyle from "../profileStyles/profileInfoStyle";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Paper from "@material-ui/core/Paper";
+import React, { useState } from 'react';
+import profileInfoStyle from '../profileStyles/profileInfoStyle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import ImageUpload from './ImageUpload';
+import Paper from '@material-ui/core/Paper';
 
-const InfoList = ({info})=>{
-    const classes = profileInfoStyle();
-    return (
-        <Paper className={classes.paper} key={info.id}>
-        <h2> {info.username}'s profile </h2>
-        <List className={classes.root}>
-          <ListItem>
-            <ListItemText primary="First Name" secondary={info.first_name} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="Last Name" secondary={info.last_name} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="Email" secondary={info.email} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="Username" secondary={info.username} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="Date of Birth" secondary={info.dob} />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="Address" secondary={info.address} />
-          </ListItem>
-          <ListItem>
-            <ListItemText secondary={info.city} />
-            <ListItemText secondary={info.state} />
-            <ListItemText secondary={info.zip} />
-          </ListItem>
-        </List>
-      </Paper>
-    )
-}
+const InfoList = ({ info }) => {
+  const classes = profileInfoStyle();
+  return (
+    <Paper className={classes.paper} key={info.id}>
+      <h2> {info.username}'s profile </h2>
+      <List className={classes.root}>
+        <ListItem>
+          <Avatar src={info.image_url} alt='' className={classes.large} />
+        </ListItem>
+        <ListItem>
+          <ImageUpload info={info} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary='First Name' secondary={info.first_name} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary='Last Name' secondary={info.last_name} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary='Email' secondary={info.email} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary='Username' secondary={info.username} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary='Date of Birth' secondary={info.dob} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary='Address' secondary={info.address} />
+        </ListItem>
+        <ListItem>
+          <ListItemText secondary={info.city} />
+          <ListItemText secondary={info.state} />
+          <ListItemText secondary={info.zip} />
+        </ListItem>
+      </List>
+    </Paper>
+  );
+};
 export default InfoList;
--- src/components/pages/profileStyles/profileInfoStyle.js
+++ src/components/pages/profileStyles/profileInfoStyle.js
@@ -1,14 +1,19 @@
-import { makeStyles } from "@material-ui/core/styles";
-const profileInfoStyle = makeStyles((theme) => ({ 
-    root: {
-        width: "100%",
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-      },
-      paper: {
-        padding: theme.spacing(1),
-        textAlign: "center",
-        width: "100%",
-      },
+import { makeStyles } from '@material-ui/core/styles';
+const profileInfoStyle = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    width: '100%'
+  },
+  large: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+    margin: 'auto'
+  }
 }));
 export default profileInfoStyle;
