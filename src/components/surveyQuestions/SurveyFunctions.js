a7817ac3067319dd1b8e4370232feea40ab60aa8
ANA TULEA <anatulea14@gmail.com>
2020-07-13 16:49:02 -0700

    Fixed the token error for survey page

--- src/components/surveyQuestions/SurveyFunctions.js
+++ src/components/surveyQuestions/SurveyFunctions.js
@@ -8,21 +8,18 @@ const getSurveyData = async () => {
   return response.data;
 };
 
-const putSurveyAnswers = async (answers, userId ) =>{
-
+const putSurveyAnswers = async (answers, userId) => {
   axiosWithAuth()
-  .put(`https://production-juxta-city-be.herokuapp.com/api/profile/${userId}/answers`, 
-  answers
-  )
-  .then((res)=>{
-      console.log(res)
-  })
-  .catch((err) => {
+    .put(
+      `https://production-juxta-city-be.herokuapp.com/api/profile/${userId}/answers`,
+      answers
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
       console.log("error", err);
     });
-}
-
-export {
-  getSurveyData,
-  putSurveyAnswers
 };
+
+export { getSurveyData, putSurveyAnswers };
