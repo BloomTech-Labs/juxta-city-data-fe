b7474d047d17d5c3a8c2c329a5673b1c2ba15792
Conary36 <cbeckford144@gmail.com>
2020-08-01 18:03:12 -0400

    refresh init

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ import SurveyQuestions from "./components/surveyQuestions/SurveyQuestions.js";
 import RecomendedDashboard from "./components/recomended/RecomendedDashboard";
 
 const AppDiv = styled.div`
-  max-width: 1280px;
-  width: 100%;
+  max-width: 100%;
   margin: 0 auto;
 `;
 const Darken = styled.div`
