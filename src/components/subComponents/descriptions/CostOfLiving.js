d0037f7b1877b5abcfd2ff41985cb078ad26629a
Ana Tulea <anatulea14@gmail.com>
2020-08-04 16:20:27 -0700

    Added statistic tag to costofliving.js

--- src/components/subComponents/descriptions/CostOfLiving.js
+++ src/components/subComponents/descriptions/CostOfLiving.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React from 'react';
 
-import { Grid} from "semantic-ui-react";
+import { Grid, Statistic } from 'semantic-ui-react';
 
 export default function CostOfLiving(props) {
-    let hdti = Math.floor((props.cityData.HDTI)*100)/100
+  let hdti = Math.floor(props.cityData.HDTI * 100) / 100;
   return (
-    <Grid columns="equal" className="box-container">
+    <Grid columns='equal' className='box-container'>
       <h2>Cost of living </h2>
       <Grid.Row id={props.title.trim()}>
         <Grid.Column>
-          <h3>Cost of living score </h3>
-          <h4>{props.cityData.Cost_of_Living_Index}</h4>
+          <Statistic size='tiny'> <h3>Cost of living score</h3>
+            <Statistic.Value id='value'>{props.cityData.Cost_of_Living_Index} </Statistic.Value>
+          </Statistic>
         </Grid.Column>
         <Grid.Column>
-          <h3>Median house value</h3>
-          <h4>$ {props.cityData.Median_House_Value}</h4>
-          <p> {hdti} dept/income ratio</p>
+          <Statistic size='tiny'><h3>Median house value </h3> <Statistic.Value>$ {props.cityData.Median_House_Value}
+            </Statistic.Value>
+            <Statistic size='mini'> <Statistic.Value>{hdti}</Statistic.Value>
+              <Statistic.Label> dept/income ratio</Statistic.Label>
+            </Statistic>
+          </Statistic>
         </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
+      </Grid.Row>
+      <Grid.Row>
         <Grid.Column>
-          <h3>Median Rent </h3>
-          <h4>$ {props.cityData.Median_Rent}</h4>
+          <Statistic size='tiny'> <h3>Median Rent </h3> 
+          <Statistic.Value>$ {props.cityData.Median_Rent}</Statistic.Value>
+          </Statistic>
         </Grid.Column>
         <Grid.Column>
-          <h3>Commute time </h3>
-          <h4>{props.cityData.Average_Commute_Time} min</h4>
+          <Statistic size='tiny'> <h3>Commute time </h3>
+            <Statistic.Value> {props.cityData.Average_Commute_Time}</Statistic.Value>
+            <Statistic.Label>min</Statistic.Label>
+          </Statistic>
         </Grid.Column>
       </Grid.Row>
     </Grid>
