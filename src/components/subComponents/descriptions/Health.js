f01d87ffaaf54168110d75475fb470fe130f7e4f
Ana Tulea <anatulea14@gmail.com>
2020-08-05 13:05:32 -0700

    refactored Health.js to fix accessability error

--- src/components/subComponents/descriptions/Health.js
+++ src/components/subComponents/descriptions/Health.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
+import { Header, Button, Icon } from 'semantic-ui-react';
 
 const Health = props => (
-  <List size='huge' id='box-container' >
-    <List.Item id={props.title.trim()}>
-      <h2>Health Data</h2>
-      <List.Description>
-        <a target='blank' href='https://findyourcity.herokuapp.com'>
-          Click here{' '}
-        </a>
-        for an interactive map on heart disease
-      </List.Description>
-    </List.Item>
-  </List>
+  <div id='box-container'>
+    <Header as='h2' id={props.title.trim()}>
+      Health Data
+      <Header.Subheader>
+        Take a look at our interactive map on heart disease by United States
+        county
+      </Header.Subheader>
+      <Button aria-hidden="false"
+        style={{ backgroundColor: '#1890FF', color: 'black' }}
+        as='a'target='blank'
+        href='https://findyourcity.herokuapp.com'> View Map
+        <Icon
+          name='long arrow alternate right'
+          aria-hidden="false"/>
+      </Button>
+    </Header>
+  </div>
 );
 
 export default Health;
