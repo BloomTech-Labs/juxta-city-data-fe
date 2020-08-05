0929aa3b87037156b81465899e8eacc69daedcf4
Ana Tulea <anatulea14@gmail.com>
2020-08-04 19:09:14 -0700

    Shorten population component

--- src/components/subComponents/descriptions/Population.js
+++ src/components/subComponents/descriptions/Population.js
@@ -1,47 +1,29 @@
 import React from 'react';
-import { Grid, Statistic } from 'semantic-ui-react';
+import { Statistic } from 'semantic-ui-react';
 
 export default function Population(props) {
-  return (
-    <Grid columns='equal' id='box-container'>
-      <h2>Population </h2>
-      <Grid.Row id={props.title.trim()}>
-        <Grid.Column>
-          <Statistic size='tiny'>
-            <h3 className='h5'>Population </h3>
-            <Statistic.Value id='value'>
-              {props.data.population}
-            </Statistic.Value>
-            <Statistic.Label>People</Statistic.Label>
-            <p>as of 2017</p>
-            <Statistic size='mini'>
-              <Statistic.Value>
-                {props.data.population_change}%{' '}
-              </Statistic.Value>
-              <p>since 2000</p>
-            </Statistic>
-          </Statistic>
-        </Grid.Column>
-        <Grid.Column>
-          <Statistic size='tiny'>
-            <h3>Density </h3>
-            <Statistic.Value>{props.data.Population_Density}</Statistic.Value>
-            <Statistic.Label>people/mi2</Statistic.Label>
-            <Statistic size='mini'>
-              <Statistic.Value>1,304</Statistic.Value>
-              <Statistic.Label>people/mi2</Statistic.Label>
-              <p>optimal Density</p>
-            </Statistic>
-          </Statistic>
-        </Grid.Column>
-        <Grid.Column>
-          <Statistic size='tiny'>
-            <h3>Median age </h3>
-            <Statistic.Value>{props.data.Median_Age}</Statistic.Value>
-            <Statistic.Label>years</Statistic.Label>
-          </Statistic>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+  return ( 
+  <div id='box-container'>
+     <h2 >Population </h2>
+      <Statistic.Group size='small' > 
+     <Statistic><h3 >Population </h3>
+       <Statistic.Value>{props.data.population}</Statistic.Value>
+       <Statistic.Label>People</Statistic.Label> <p>as of 2017</p>
+       <Statistic >
+         <Statistic.Value> {props.data.population_change}%</Statistic.Value><p>since 2000</p>
+       </Statistic>
+     </Statistic>
+     <Statistic > <h3>Density </h3>
+       <Statistic.Value>{props.data.Population_Density}</Statistic.Value>
+       <Statistic.Label>people/mi2</Statistic.Label>
+       <Statistic >
+         <Statistic.Value>1,304</Statistic.Value> <Statistic.Label>people/mi2</Statistic.Label><p>optimal Density</p>
+       </Statistic>
+     </Statistic>
+     <Statistic ><h3>Median age </h3>
+       <Statistic.Value>{props.data.Median_Age}</Statistic.Value><Statistic.Label>years</Statistic.Label>
+     </Statistic>
+     </Statistic.Group>
+   </div>
   );
-}
+};
