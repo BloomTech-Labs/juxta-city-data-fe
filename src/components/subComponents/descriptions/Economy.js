eb27c7f9e011ec493719a4cda9a376752bd138a0
Ana Tulea <anatulea14@gmail.com>
2020-08-05 15:17:00 -0700

    refactored similar blocks of code in economy component

--- src/components/subComponents/descriptions/Economy.js
+++ src/components/subComponents/descriptions/Economy.js
@@ -4,21 +4,20 @@ import { Statistic } from 'semantic-ui-react';
 export default function EconomyBox(props) {
 
   const incomeData = [{ title: 'Household Median Income', value: props.data.Median_Income },
-  { title: 'Person average Income ', value: props.data.per_capita_Income }];
-
-  const povertyData = [{ rate: 'Poverty Rate', percentage: props.data.Percent_below_Poverty },
+  { title: 'Person average Income ', value: props.data.per_capita_Income },
+  { rate: 'Poverty Rate', percentage: props.data.Percent_below_Poverty },
   { rate: 'Unemployement Rate ', percentage: props.data.Unemployment_rate }];
 
   return (
     <div id='box-container'>
       <h2>Economy </h2>
         <Statistic.Group size='tiny' id={props.title.trim()}> 
-          {incomeData.map((item)=>(
+          {incomeData.slice(0, 2).map((item)=>(
             <Statistic className="card-stats"><h3>{item.title}</h3>
               <Statistic.Value> ${item.value}</Statistic.Value>
               <Statistic.Label>per year</Statistic.Label> </Statistic>))}
 
-          {povertyData.map((item)=>(
+          {incomeData.slice(2, 4).map((item)=>(
             <Statistic className="card-stats"> <h3>{item.rate}</h3>
               <Statistic.Value>{item.percentage} % </Statistic.Value> </Statistic> ))}
           
