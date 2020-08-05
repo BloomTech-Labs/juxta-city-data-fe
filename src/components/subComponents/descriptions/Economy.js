de0bc699395be52ae130977762768f495e9a8327
Ana Tulea <anatulea14@gmail.com>
2020-08-04 22:42:03 -0700

    refactored economy component

--- src/components/subComponents/descriptions/Economy.js
+++ src/components/subComponents/descriptions/Economy.js
@@ -2,26 +2,30 @@ import React from 'react';
 import { Statistic } from 'semantic-ui-react';
 
 export default function EconomyBox(props) {
+
+  const incomeData = [{ title: 'Household Median Income', value: props.data.Median_Income },
+  { title: 'Person average Income ', value: props.data.per_capita_Income }];
+
+  const povertyData = [{ title: 'Poverty Rate', value: props.data.Percent_below_Poverty },
+  { title: 'Unemployement Rate ', value: props.data.Unemployment_rate }];
+
   return (
     <div id='box-container'>
       <h2>Economy </h2>
         <Statistic.Group size='tiny' id={props.title.trim()}> 
-          <Statistic className="card-stats"><h3>Household Median Income</h3>
-            <Statistic.Value> ${props.data.Median_Income}</Statistic.Value>
-            <Statistic.Label>per year</Statistic.Label> </Statistic>
-          <Statistic className="card-stats"><h3>Person average Income</h3>
-            <Statistic.Value>${props.data.per_capita_Income} </Statistic.Value>
-            <Statistic.Label>per year</Statistic.Label> </Statistic>
-          <Statistic className="card-stats"> <h3>Poverty Rate</h3>
-            <Statistic.Value>{props.data.Percent_below_Poverty} % </Statistic.Value>
-          </Statistic>
-          <Statistic className="card-stats"><h3>Unemployement Rate</h3>
-            <Statistic.Value>{props.data.Unemployment_rate} % </Statistic.Value>
-          </Statistic>
-        {props.data.Crime_rate === 'No Data Available' ? null : (
-            <Statistic className="card-stats"><h3>Crime Rate</h3>
-              <Statistic.Value>{props.data.Crime_rate} </Statistic.Value>
-            </Statistic> )}
+          {incomeData.map((item)=>(
+            <Statistic className="card-stats"><h3>{item.title}</h3>
+              <Statistic.Value> ${item.value}</Statistic.Value>
+              <Statistic.Label>per year</Statistic.Label> </Statistic>))}
+
+          {povertyData.map((item)=>(
+            <Statistic className="card-stats"> <h3>{item.title}</h3>
+              <Statistic.Value>{item.value} % </Statistic.Value> </Statistic> ))}
+          
+          {props.data.Crime_rate === 'No Data Available' ? null : (
+              <Statistic className="card-stats"><h3>Crime Rate</h3>
+                <Statistic.Value>{props.data.Crime_rate} </Statistic.Value>
+              </Statistic>)}
       </Statistic.Group>
    </div>
   );
