abcf3fcc9cb69b84dc2f8364d1bbbc4a6b1c32db
Ana Tulea <anatulea14@gmail.com>
2020-08-04 22:46:30 -0700

    changed key names in economy object arrays

--- src/components/subComponents/descriptions/Economy.js
+++ src/components/subComponents/descriptions/Economy.js
@@ -6,8 +6,8 @@ export default function EconomyBox(props) {
   const incomeData = [{ title: 'Household Median Income', value: props.data.Median_Income },
   { title: 'Person average Income ', value: props.data.per_capita_Income }];
 
-  const povertyData = [{ title: 'Poverty Rate', value: props.data.Percent_below_Poverty },
-  { title: 'Unemployement Rate ', value: props.data.Unemployment_rate }];
+  const povertyData = [{ rate: 'Poverty Rate', percentage: props.data.Percent_below_Poverty },
+  { rate: 'Unemployement Rate ', percentage: props.data.Unemployment_rate }];
 
   return (
     <div id='box-container'>
@@ -19,8 +19,8 @@ export default function EconomyBox(props) {
               <Statistic.Label>per year</Statistic.Label> </Statistic>))}
 
           {povertyData.map((item)=>(
-            <Statistic className="card-stats"> <h3>{item.title}</h3>
-              <Statistic.Value>{item.value} % </Statistic.Value> </Statistic> ))}
+            <Statistic className="card-stats"> <h3>{item.rate}</h3>
+              <Statistic.Value>{item.percentage} % </Statistic.Value> </Statistic> ))}
           
           {props.data.Crime_rate === 'No Data Available' ? null : (
               <Statistic className="card-stats"><h3>Crime Rate</h3>
