118b3b8f5929654aea051c63e3c25d25b37b9dd6
Ana Tulea <anatulea14@gmail.com>
2020-08-05 14:28:13 -0700

    improved  EconomyGraph component

--- src/components/subComponents/industryGraph/EconomyGraph.js
+++ src/components/subComponents/industryGraph/EconomyGraph.js
@@ -1,84 +1,22 @@
-import React, { useContext } from "react";
-import CityContext from "../../../contexts/CityContext";
-import { convertStringToObject } from "./graphFunctions";
-import BarGraph from "./BarGraph";
+import React, { useContext } from 'react';
+import CityContext from '../../../contexts/CityContext';
+import { convertStringToObject, changeKeyNames } from './graphFunctions';
+import BarGraph from './BarGraph';
 
 function EconomyGraph() {
   const { cityData } = useContext(CityContext);
-  // console.log(cityData, "city data");
 
-  const economyDataString = cityData.Most_Common_Industries
+  const economyDataString = cityData.Most_Common_Industries;
   // save the data to the local storage and then we get it from there and use
-  
-  localStorage.setItem("economyDataString", economyDataString)
-  const localdata = localStorage.getItem("economyDataString") 
+
+  localStorage.setItem('economyDataString', economyDataString);
+  const localdata = localStorage.getItem('economyDataString');
   // console.log(economyDataString, " economyDataString");
 
-  const economyDataStringLocal  = localdata.split("'")
-  const economyObject = convertStringToObject(economyDataStringLocal );
+  const economyDataStringLocal = localdata.split("'");
+  const economyObject = convertStringToObject(economyDataStringLocal);
 
-  let arrOfIndustries = [];
-
-  const changeKeyNames = (obj) => {
-    for (let keyName in obj) {
-      let xlabel;
-      function shortenIndustryName(keyName) {
-        let translation = [];
-
-        translation["Health care "] = "Health";
-        translation["Professional, scientific, technical services "] = "Tech";
-        translation["Educational services "] = "Education";
-        translation["Accommodation & food services "] = "Food";
-        translation["Construction "] = "Construction";
-        translation["Chemicals "] = "Chemicals";
-        translation["Administrative & support & waste management services "] =
-          "Maintenance";
-        translation["Public administration "] = "Public admin";
-        translation["Transportation equipment "] = "Transportation";
-        translation[
-          "Publishing, motion picture & sound recording industries "
-        ] = "Publishing";
-        translation["Food & beverage stores "] = "Stores";
-        translation["Social assistance "] = "Assistance";
-        translation["Finance & insurance "] = "Finance";
-        translation["Metal & metal products "] = "Metal";
-        translation["Utilities "] = "Utilities";
-        translation["Computer & electronic products "] = "Electronics";
-        translation["Truck transportation "] = "Transportation";
-        translation["Mining, quarrying, oil & gas extraction "] = "Extraction";
-        translation["Department & other general merchandise stores "] =
-          "Merchandise";
-        translation["Air transportation "] = "Transportation";
-        translation["Textile mills & textile products "] = "Textile";
-        translation["Agriculture, forestry, fishing & hunting "] =
-          "Agriculture";
-        translation["Apparel "] = "Apparel";
-        translation["Repair & maintenance "] = "Maintenance";
-        translation["Food "] = "Food";
-        translation["Paper "] = "Paper";
-        translation["Broadcasting & telecommunications "] = "Broadcasting";
-        translation[
-          "Used merchandise, gift, novelty, souvenir, other miscellaneous stores "
-        ] = "Miscellaneous";
-        translation["Data processing, libraries, other information services "] =
-          "Information";
-          translation["Arts, entertainment, recreation "] = "Art";
-          translation["Real estate & rental & leasing "] = "Estate";
-          
-        xlabel = translation[keyName];
-      }
-     
-      shortenIndustryName(keyName);
-
-      arrOfIndustries.push({
-        name: keyName,
-        Industry: parseFloat(obj[keyName]),
-        xlabel: xlabel,
-      });
-    }
-  };
-  // console.log(arrOfIndustries, "arrOfIndustries");
-  changeKeyNames(economyObject);
+  let arrOfIndustries = changeKeyNames(economyObject);
 
   return <BarGraph arrOfIndustries={arrOfIndustries} />;
 }
--- src/components/subComponents/industryGraph/graphFunctions.js
+++ src/components/subComponents/industryGraph/graphFunctions.js
@@ -3,7 +3,7 @@ import React from 'react';
 const convertStringToObject = arr => {
   let keyValueArray = [];
   let arrOfKeyValueArrays = [];
-  // let arrParsed =  arr.split(" ' ");
+
 
   arr.map(element => {
     element.split("'").map(item => {
@@ -47,7 +47,72 @@ function CustomTooltip({ payload, label, active }) {
 }
 
 const renderCustomBarLabel = ({ x, y, width, value }) => {
-  return <text x={x + width / 2} y={y} fill='#000' dy={-6} >{`${value}%`}</text>;
+  return <text x={x + width / 2} y={y} fill='#000' dy={-6}>{`${value}%`}</text>;
 };
 
-export { convertStringToObject, renderCustomBarLabel, CustomTooltip };
+const changeKeyNames = obj => {
+  let arrOfIndustries = [];
+
+  for (let keyName in obj) {
+    let xlabel;
+    function shortenIndustryName(keyName) {
+      let translation = [];
+
+      translation['Health care '] = 'Health';
+      translation['Professional, scientific, technical services '] = 'Tech';
+      translation['Educational services '] = 'Education';
+      translation['Accommodation & food services '] = 'Food';
+      translation['Construction '] = 'Construction';
+      translation['Chemicals '] = 'Chemicals';
+      translation['Administrative & support & waste management services '] =
+        'Maintenance';
+      translation['Public administration '] = 'Public admin';
+      translation['Transportation equipment '] = 'Transportation';
+      translation['Publishing, motion picture & sound recording industries '] =
+        'Publishing';
+      translation['Food & beverage stores '] = 'Stores';
+      translation['Social assistance '] = 'Assistance';
+      translation['Finance & insurance '] = 'Finance';
+      translation['Metal & metal products '] = 'Metal';
+      translation['Utilities '] = 'Utilities';
+      translation['Computer & electronic products '] = 'Electronics';
+      translation['Truck transportation '] = 'Transportation';
+      translation['Mining, quarrying, oil & gas extraction '] = 'Extraction';
+      translation['Department & other general merchandise stores '] =
+        'Merchandise';
+      translation['Air transportation '] = 'Transportation';
+      translation['Textile mills & textile products '] = 'Textile';
+      translation['Agriculture, forestry, fishing & hunting '] = 'Agriculture';
+      translation['Apparel '] = 'Apparel';
+      translation['Repair & maintenance '] = 'Maintenance';
+      translation['Food '] = 'Food';
+      translation['Paper '] = 'Paper';
+      translation['Broadcasting & telecommunications '] = 'Broadcasting';
+      translation[
+        'Used merchandise, gift, novelty, souvenir, other miscellaneous stores '
+      ] = 'Miscellaneous';
+      translation['Data processing, libraries, other information services '] =
+        'Information';
+      translation['Arts, entertainment, recreation '] = 'Art';
+      translation['Real estate & rental & leasing '] = 'Estate';
+
+      xlabel = translation[keyName];
+    }
+
+    shortenIndustryName(keyName);
+
+    arrOfIndustries.push({
+      name: keyName,
+      Industry: parseFloat(obj[keyName]),
+      xlabel: xlabel,
+    });
+  }
+  return arrOfIndustries;
+};
+
+export {
+  convertStringToObject,
+  renderCustomBarLabel,
+  CustomTooltip,
+  changeKeyNames,
+};
