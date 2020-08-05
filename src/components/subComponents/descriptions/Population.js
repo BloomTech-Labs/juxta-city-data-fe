ea1e19d5e22666154664ba6ac61fd3209a5d8674
Ana Tulea <anatulea14@gmail.com>
2020-08-04 20:57:49 -0700

    removed background image and made cards

--- src/components/subComponents/descriptions/Population.js
+++ src/components/subComponents/descriptions/Population.js
@@ -6,21 +6,21 @@ export default function Population(props) {
   <div id='box-container'>
      <h2 >Population </h2>
       <Statistic.Group size='small' id={props.title.trim()}> 
-     <Statistic><h3 >Population </h3>
+     <Statistic className="card-stats"><h3 >Population </h3>
        <Statistic.Value>{props.data.population}</Statistic.Value>
        <Statistic.Label>People</Statistic.Label> <p>as of 2017</p>
        <Statistic >
          <Statistic.Value> {props.data.population_change}%</Statistic.Value><p>since 2000</p>
        </Statistic>
      </Statistic>
-     <Statistic > <h3>Density </h3>
+     <Statistic className="card-stats"> <h3>Density </h3>
        <Statistic.Value>{props.data.Population_Density}</Statistic.Value>
        <Statistic.Label>people/mi2</Statistic.Label>
-       <Statistic >
+       <Statistic>
          <Statistic.Value>1,304</Statistic.Value> <Statistic.Label>people/mi2</Statistic.Label><p>optimal Density</p>
        </Statistic>
      </Statistic>
-     <Statistic ><h3>Median age </h3>
+     <Statistic className="card-stats"><h3>Median age </h3>
        <Statistic.Value>{props.data.Median_Age}</Statistic.Value><Statistic.Label>years</Statistic.Label>
      </Statistic>
      </Statistic.Group>
