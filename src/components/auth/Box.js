e21c07ef075364b996564695e586df17b7679718
ANA TULEA <anatulea14@gmail.com>
2020-08-10 15:41:30 -0700

    formated box

--- src/components/auth/Box.js
+++ src/components/auth/Box.js
@@ -4,23 +4,28 @@ import { Card } from "semantic-ui-react";
 import LogoHeader from "./LogoHeader";
 import SignInInputs from "./SignInInputs";
 
-const Box = ({ signUpChange, handleSubmit, form, name,  signInChange,  signInForm }) => {
+const Box = ({ signUpChange, handleSubmit, form, name, signInChange, signInForm }) => {
   return (
     <Card className="ui centered cards" style={{ padding: "20px" }}>
-      {(name === "signup") ?(<>
-      <LogoHeader title="Registration" />
-      <SignUpInputs
-        signUpChange={signUpChange}
-        handleSubmit={handleSubmit}
-        form={form}
-      /></>):( <>
-      <LogoHeader title="Sign In" />
-      <SignInInputs
-        signInChange={signInChange}
-        handleSubmit={handleSubmit}
-        signInForm={signInForm}
-      /></>)
-}
+      {name === "signup" ? (
+        <>
+          <LogoHeader title="Registration" />
+          <SignUpInputs
+            signUpChange={signUpChange}
+            handleSubmit={handleSubmit}
+            form={form}
+          />
+        </>
+      ) : (
+        <>
+          <LogoHeader title="Sign In" />
+          <SignInInputs
+            signInChange={signInChange}
+            handleSubmit={handleSubmit}
+            signInForm={signInForm}
+          />
+        </>
+      )}
     </Card>
   );
 };
