f0d54c484cf5ba59b8e066295bbaa836aff52fdb
ANA TULEA <anatulea14@gmail.com>
2020-08-10 16:41:58 -0700

    refactored signin tests

--- src/components/auth/__tests__/SignIn.test.js
+++ src/components/auth/__tests__/SignIn.test.js
@@ -1,43 +1,59 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import SignIn from '../SignIn.js';
-import { TRUE } from 'node-sass';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import SignIn from "../SignIn.js";
+import { TRUE } from "node-sass";
+import ModalContext from "../../../contexts/ModalContext";
 
-describe('<SignIn />', () => {
-    it('renders without crashing', () => {
-        render(<SignIn />);
-    })
+describe("<SignIn />", () => {
+  const modal = "";
+  it("renders without crashing", () => {
+    render(
+      <ModalContext.Provider value={{ modal }}>
+        <SignIn />
+      </ModalContext.Provider>
+    );
+  });
+});
 
-})
+describe("input values", () => {
+  it("Username updates on change", () => {
+    const modal = "";
+    const { queryByPlaceholderText } = render(
+        <ModalContext.Provider value={{ modal }}>
+        <SignIn />
+      </ModalContext.Provider>
+    );
+    const typeInput = queryByPlaceholderText("username");
+    fireEvent.change(typeInput, { target: { value: "test" } });
+    expect(typeInput.value).toBe("test");
+  });
 
-describe("input values", () => {    
-    it('Username updates on change', () => {
-        const {queryByPlaceholderText} = render(<SignIn />)
-        const typeInput = queryByPlaceholderText('Username')
-        fireEvent.change(typeInput, {target : {value: "test"}})
-        expect(typeInput.value).toBe("test")
-    })
+  it("Password updates on change", () => {
+    const modal = "";
+    const { queryByPlaceholderText } = render(
+        <ModalContext.Provider value={{ modal }}>
+        <SignIn />
+      </ModalContext.Provider>);
+    const typeInput = queryByPlaceholderText("password");
+    fireEvent.change(typeInput, { target: { value: "test" } });
+    expect(typeInput.value).toBe("test");
+  });
 
-    it('Password updates on change', () => {
-        const {queryByPlaceholderText} = render(<SignIn />)
-        const typeInput = queryByPlaceholderText('Password')
-        fireEvent.change(typeInput, {target : {value: "test"}})
-        expect(typeInput.value).toBe("test")
-    })
+  it('calls "onClick" prop on button click', () => {
+    const modal = "";
+    const { queryByPlaceholderText } = render(
+        <ModalContext.Provider value={{ modal }}>
+        <SignIn />
+      </ModalContext.Provider>
+    );
+    const handleSubmit = jest.fn();
+    const passwordInput = queryByPlaceholderText("password");
+    const usernameInput = queryByPlaceholderText("username");
 
-    it('calls "onClick" prop on button click', () => {
-        
-        const {queryByPlaceholderText} = render(<SignIn />)
-        const handleSubmit = jest.fn();
-        const passwordInput = queryByPlaceholderText('Password');
-        const usernameInput = queryByPlaceholderText('Username')
-
-        if(usernameInput && passwordInput === TRUE){
-        const { getByText } = render(<SignIn onClick={handleSubmit} />);      
-        fireEvent.click(getByText(/submit/i));
-        expect(handleSubmit).toHaveBeenCalled();
-        }
-        
-      });
-
-})
+    if (usernameInput && passwordInput === TRUE) {
+      const { getByText } = render(<SignIn onClick={handleSubmit} />);
+      fireEvent.click(getByText(/sign in/i));
+      expect(handleSubmit).toHaveBeenCalled();
+    }
+  });
+});
