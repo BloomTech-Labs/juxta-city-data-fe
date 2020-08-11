2ebf9c1d97f1cf0a9504dcbe3654462c6417a64b
ANA TULEA <anatulea14@gmail.com>
2020-08-11 15:50:06 -0700

    Refactored NavBar using Semantic ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,12 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
+import { Menu, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import Logo from "../assets/logo.png";
-import LogoWhite from "../assets/logo-white.png";
-import UserContext from "../contexts/UserContext";
-import { makeStyles } from "@material-ui/core/styles";
 import poly from "../assets/polydown.png";
-import polyWhite from "../assets/polyWhite.png";
 import avatar from "../assets/avatar.png";
+import UserContext from "../contexts/UserContext";
 import ProfileContext from "../contexts/ProfileContext";
 
 const UL = styled.ul`
@@ -20,18 +18,8 @@ const UL = styled.ul`
   justify-content: flex-end;
 `;
 
-// const H2 = styled.h2`
-//   width: 25%;
-//   margin-left: 50px;
-//   justify-content: flex-start;
-//   padding-right: 30px;
-//   @media (max-width: 500px) {
-//     margin: auto 0;
-//   }
-// `;
-
 const Li = styled.li`
-  width: 18%;
+  width: 15%;
   text-decoration: none;
   max-padding: 0 20px;
   margin: 0 15px;
@@ -51,121 +39,25 @@ const NavDiv = styled.div`
   width: 100%;
   background: #f2f2f2;
 `;
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 130,
-    height: 170,
-    zIndex: 3,
-    marginTop: 100,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    borderRadius: 4,
-    [theme.breakpoints.down("sm")]: {
-      right: 16,
-    },
-  },
-  avatarBox: {
-    display: "flex",
-    alignItems: "center",
-  },
-  modalLi: {
-    padding: "7px 0",
-    textDecoration: "none",
-    listStyleType: "none",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: 16,
-    "&:hover": {
-      cursor: "pointer",
-      fontWeight: "bold",
-    },
-  },
-  darken: {
-    position: "absolute",
-    zIndex: 2,
-    border: "1px solid red",
-    width: `100vh`,
-    height: `100vh`,
-    background: "rgba(0,0,0,0.5)",
-    top: 80,
-    transition: "ease-in-out 2s",
-    [theme.breakpoints.down("sm")]: {
-      top: 80,
-    },
-  },
-  animation: {
-    transition: "ease-in .2s",
-    width: 15,
-    height: 10,
-    "&:hover": {
-      cursor: "pointer",
-    },
-  },
-  animation2: {
-    transition: "ease-in .2s",
-    transform: "rotate(180deg)",
-    width: 15,
-    height: 10,
-    "&:hover": {
-      cursor: "pointer",
-    },
-  },
-  SignInBlue: {
-    border: "0",
-    background: "none",
-    fontSize: 16,
-    color: "white",
-    [theme.breakpoints.down("sm")]: {
-      background: "#687FAD",
-      color: "white",
-      fontSize: 16,
-      border: "0",
-    },
-    "&:hover": {
-      cursor: "pointer",
-    },
-  },
-  SignInWhite: {
-    border: "0",
-    background: "none",
-    fontSize: 16,
-    [theme.breakpoints.down("sm")]: {
-      background: "#687FAD",
-      color: "white",
-      fontSize: 16,
-      border: "0",
-    },
-    "&:hover": {
-      cursor: "pointer",
-    },
-  },
-}));
 
 const NavBar = ({ auth, history, location }) => {
-  const classes = useStyles();
   const { setUserData } = useContext(UserContext);
   const { setProfileData } = useContext(ProfileContext);
   const [open, setOpen] = useState(false);
-  const { profileData } = useContext(ProfileContext)
+
   const login = () => {
     history.push("/signin");
   };
+ 
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   const handleDash = () => {
     handleOpen();
     history.push("/dashboard");
   };
-  const handleOpen = () => {
-    let background = document.getElementById("darken");
-    if (open) {
-      background.style.display = "none";
-    } else {
-      background.style.display = "block";
-    }
-    setOpen(!open);
-  };
-
+  
   const handleAbout = () => {
     history.push("/");
     handleOpen();
@@ -188,32 +80,23 @@ const NavBar = ({ auth, history, location }) => {
   };
 
   const body = (
-    <div className={classes.paper}>
-      <ul className={classes.modalLi}>
-        <li className={classes.modalLi}>
+    <div className="nav-menu">
+      <Menu pointing vertical style={{ position: "absolute", width: "140px" }}>
+        <Menu.Item>
           <Link to="/profile">Profile</Link>
-        </li>
-        <li className={classes.modalLi} onClick={handleAbout}>
-          About
-        </li>
-        <li className={classes.modalLi} onClick={handleDash}>
-          Dashboard
-        </li>
-        <li className={classes.modalLi} onClick={logout}>
-          Logout
-        </li>
-      </ul>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/survey">Survey</Link>
+        </Menu.Item>
+        <Menu.Item onClick={handleAbout}> About</Menu.Item>
+        <Menu.Item onClick={handleDash}> Dashboard</Menu.Item>
+        <Menu.Item onClick={logout}> Logout</Menu.Item>
+      </Menu>
     </div>
   );
 
   let token = localStorage.getItem("token");
-  let profileImage = localStorage.getItem("profilePicture")
-  // const [profileImage, setProfileImage] = useState("");
-
-  // useEffect(() => {
-  //   setProfileImage(localStorage.getItem("profilePicture"))
-    
-  // });
+  let profileImage = localStorage.getItem("profilePicture");
 
   return (
     <NavDiv pathname={location.pathname}>
@@ -226,35 +109,26 @@ const NavBar = ({ auth, history, location }) => {
       </Link>
       {token ? (
         <UL>
-          <Li className={classes.avatarBox}>
-            {profileImage===null ? (
-              <img
-                src={profileImage}
-                alt="avatar"
-                style={{
-                  height: "60px",
-                  width: "60px",
-                  borderRadius: "50%",
-                  border: "3px solid #191969 ",
-                }}
-              />
-            ) : (
-              <img
-                src={avatar}
-                alt="avatar"
-                style={{
-                  height: "60px",
-                  width: "60px",
-                  borderRadius: "50%",
-                  border: "3px solid #191969 ",
-                }}
-              />
-            )}
-
+          <Li className="avatarBox">
+            <img
+              src={
+                !((profileImage === "null") | (profileImage === null))
+                  ? profileImage
+                  : avatar
+              }
+              alt="profile avatar"
+              onClick={handleOpen}
+              style={{
+                height: "60px",
+                width: "60px",
+                borderRadius: "50%",
+                border: "3px solid #191969 ",
+              }}
+            />
             <button className="link" onClick={handleOpen}>
               <img
                 id="dropdown"
-                className={!open ? classes.animation : classes.animation2}
+                // className={!open ? classes.animation : classes.animation2}
                 src={poly}
                 alt="navigation arrow here"
               />
@@ -266,18 +140,7 @@ const NavBar = ({ auth, history, location }) => {
       ) : (
         <UL>
           <Li>
-            <button
-              className={
-                location.pathname === "/cityview"
-                  ? classes.SignInBlue
-                  : location.pathname === "/recommended"
-                  ? classes.SignInBlue
-                  : classes.SignInWhite
-              }
-              onClick={login}
-            >
-              Sign In
-            </button>
+            <Button onClick={login}>Sign In</Button>
           </Li>
         </UL>
       )}
