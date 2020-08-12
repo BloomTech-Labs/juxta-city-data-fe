607cc8288358f4044d0d79b28bbd09b0a039a913
ANA TULEA <anatulea14@gmail.com>
2020-08-12 11:32:49 -0700

    minor color improvenents

--- src/components/auth/SubmitBotton.js
+++ src/components/auth/SubmitBotton.js
@@ -3,7 +3,7 @@ import { Button, Icon } from "semantic-ui-react";
 const SubmitButton = ({ title }) => {
   return (
     <Button
-      style={{ backgroundColor: "#191969", color: "white", margin: "0 60px" }}
+      style={{ backgroundColor: "#0074cc", color: "white", margin: "0 60px" }}
     >
       {title}
       <Icon name="long arrow alternate right" />
