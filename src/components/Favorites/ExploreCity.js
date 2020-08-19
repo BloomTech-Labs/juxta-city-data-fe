5cb9f531dedc4e355345643516e0bc312bc0820d
Ana Tulea <anatulea14@gmail.com>
2020-08-19 15:27:00 -0700

    refactored explore city

--- src/components/Favorites/ExploreCity.js
+++ src/components/Favorites/ExploreCity.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import SearchBar from '../subComponents/SearchBar';
-import { Button } from 'semantic-ui-react';
+import { Button, Card } from 'semantic-ui-react';
 import { useHistory } from 'react-router-dom';
 
 export default function ExploreCity() {
   const history = useHistory();
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', paddingTop: '100px', marginTop: '2rem', border:"1px solid gray"}}>
-      <h1 style={{ textAlign: 'center' }}>Search for a city</h1>
-      <SearchBar />
-      <br />
-      <h1 style={{ textAlign: 'center' }}>
-        Or take our Survey to get recommendations!
-      </h1>
-      <Button
-        onClick={() => history.push('/survey')}
-        alt='Take the survey button'
-        style={{ backgroundColor: '#0074cc', color: 'white',  width: '200px',  marginLeft: '37%'}}
-      >
-        Take the survey
-      </Button>
-    </div>
+    <Card fluid style={{ textAlign: 'center', marginTop:'20px'}}>
+      <Card.Content>
+        <Card.Header as='h1'>Search for a city</Card.Header>
+        <SearchBar /> 
+        <br></br>
+        <Card.Header as='h1' style={{  marginTop:'70px'}}>
+          Or take our Survey to get recommendations!
+        </Card.Header>
+        <Button onClick={() => history.push('/survey')}  alt='Take the survey button'
+          style={{
+            backgroundColor: '#0074cc',
+            color: 'white',
+            width: '200px',
+          }}
+        >
+          Take the survey
+        </Button>
+      </Card.Content>
+    </Card>
   );
 }
