989c307a04c87a2412da5995cf6d0550b7655749
Ana Tulea <anatulea14@gmail.com>
2020-08-19 20:56:20 -0700

    added border to search bar

--- src/components/Favorites/ExploreCity.js
+++ src/components/Favorites/ExploreCity.js
@@ -6,12 +6,12 @@ import { useHistory } from 'react-router-dom';
 export default function ExploreCity() {
   const history = useHistory();
   return (
-    <Card fluid style={{ textAlign: 'center', marginTop:'20px'}}>
+    <Card fluid style={{ textAlign: 'center', padding:'40px 0'}}>
       <Card.Content>
         <Card.Header as='h1'>Search for a city</Card.Header>
         <SearchBar /> 
         <br></br>
-        <Card.Header as='h1' style={{  marginTop:'70px'}}>
+        <Card.Header as='h1' style={{  marginTop:'40px'}}>
           Or take our Survey to get recommendations!
         </Card.Header>
         <Button onClick={() => history.push('/survey')}  alt='Take the survey button'
