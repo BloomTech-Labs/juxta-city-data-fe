6d3f862a5e5909d951c202d2db652fb85f21ed57
Conary36 <cbeckford144@gmail.com>
2020-08-22 11:21:59 -0400

    improving testing...

--- src/components/pages/profile/__tests__/ProfileInputs.test.js
+++ src/components/pages/profile/__tests__/ProfileInputs.test.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import ProfileInputs from '../ProfileInputs';
-import { configure, shallow, mount } from 'enzyme';
+import { render,  unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import renderer from 'react-test-renderer';
 import { anime } from "react-anime";
 
-// import '@testing-libarary/jest-dom/extend-expect';
-import Adapter from 'enzyme-adapter-react-16';
-// import { render, fireEvent, cleanup } from '@testing-library/react';
 
-configure({ adapter: new Adapter() })
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
 
-// afterEach(cleanup)
 
-//testing a controlled component form.
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
 
 describe('the profile input ', () => {
-    const wrapper = shallow(<ProfileInputs />)
+    const wrapper = <ProfileInputs />
 
     it('snap shot the component', () => {
         const shot = renderer.create(<ProfileInputs />).toJSON()
@@ -27,22 +35,7 @@ describe('the profile input ', () => {
         expect(wrapper.find('svg')).toHaveLength(1)
     })
 
-    // it('expects wrapper to check the handleChange', () => {
-    //     const onFocus = mount(<ProfileInputs />)
-    //     console.log(onFocus.instance());
-
-    //     //onFocus.instance().handleTextFocus = jest.fn()
-
-    //     //const focus =  jest.fn();
-        
-    //     //onFocus.find('input').simulate('dob')
-
-    //     //expect(focus).toHaveBeenCalled(1)
-        
-    //     })
-
-        
-        
 
 
-    })
+})
+
