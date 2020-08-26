fc3b09bfefc27a214245f83e3d4868b8db8d7de6
Ana Tulea <anatulea14@gmail.com>
2020-08-26 16:25:06 -0700

    refactored survey tests to pass

--- src/components/pages/profile/surveyAnswers/__tests__/SurveyAnswersBox.test.js
+++ src/components/pages/profile/surveyAnswers/__tests__/SurveyAnswersBox.test.js
@@ -2,8 +2,12 @@ import React from "react";
 import { render } from "@testing-library/react";
 import SurveyAnswersBox from "../surveyAnswersBox";
 import ProfileContext from "../../../../../contexts/ProfileContext";
+import { getSurveyData } from '../../../../surveyQuestions/SurveyFunctions';
+import axios from 'axios';
 
-it("renders without crashing", () => {
+jest.mock('axios');
+
+it("renders without crashing", async () => {
   let profileData = [{
     address: "123 street st",
     city: "Chicago",
@@ -29,12 +33,42 @@ it("renders without crashing", () => {
     zip: 12543,
   }];
   const setProfileData = (data) => (profileData = data);
+ const surveyQues = [
+  {
+    id: 1,
+    question: "What is an acceptable cost of living index for your new city?",
+    name: "cost_of_living",
+    options: [
+      { id: 1, description: "Very low 75", value: "1" },
+      { id: 2, description: "Low 76 - 99", value: "2" },
+      { id: 3, description: "Medium 100 - 110", value: "3" },
+      { id: 4, description: "High 111 - 149", value: "4" },
+      { id: 5, description: "Very high 150+", value: "5" },
+    ],
+  },
+  {
+    id: 2,
+    question: "What is your age group?",
+    name: "median_age",
+    options: [
+      { id: 6, description: "17 and younger", value: "1" },
+      { id: 7, description: "18 - 29", value: "2" },
+      { id: 8, description: "30 - 46", value: "3" },
+      { id: 9, description: "47 - 65", value: "4" },
+      { id: 10, description: "66+", value: "5" },
+    ],
+  }
+]
+
+axios.get.mockResolvedValue({ data: surveyQues });
+const surveyData = await getSurveyData();
 
   let comp = render(
     <ProfileContext.Provider value={{ profileData, setProfileData }}>
-      <SurveyAnswersBox />
+      <SurveyAnswersBox surveyData={surveyData} profileData={profileData}/>
     </ProfileContext.Provider>
   );
-
+  expect(surveyData).toEqual(surveyQues);
   expect(comp.getByText(/Your Survey Answers/i)).toBeInTheDocument();
+  expect(comp.getByText(/Retake the survey/i)).toBeInTheDocument();
 });
